1439ba55458a8e67cf6e1a02554ec951e0710d20
saveole <xgz.cz@foxmail.com>
2023-08-15 17:39:53 +0800

    syscall usage with golang.org/x/sys/unix

---
+++ ch1/syscall/go.mod
@@ -0,0 +1,5 @@
+module Software-Development-with-Go/ch1/syscall
+
+go 1.20
+
+require golang.org/x/sys v0.11.0
---
+++ ch1/syscall/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	u "golang.org/x/sys/unix"
+)
+
+func main() {
+	c := make([]byte, 512)
+
+	log.Println("Getpid:", u.Getpid())
+	log.Println("Getpgrp:", u.Getpgrp())
+	log.Println("Getppid:", u.Getppid())
+	log.Println("Gettid:", u.Gettid())
+
+	_, err := u.Getcwd(c)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(c))
+
+}
